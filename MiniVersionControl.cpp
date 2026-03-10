ca14a1723ed074756765fa17167666581321bebd
Samuel Horner <Samuelhorner123@gmail.com>
2026-03-10 12:47:05 -0400

    Add project files.

---
+++ MiniVersionControl.cpp
@@ -0,0 +1,12 @@
+﻿// MiniVersionControl.cpp : Defines the entry point for the application.
+//
+
+#include "MiniVersionControl.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
