e08576f8516a79ccab05de382995a88ca66b5d78
Spencer Scarlett <scar0064@algonquinlive.com>
2026-04-04 13:36:14 -0400

    working on saving, half way done

--- src/DataManager.cpp
+++ src/DataManager.cpp
@@ -6,47 +6,71 @@
 #include <iomanip>
 #include <filesystem>
 #include <sstream>
-
+#include <filesystem>
+#include <ctime>
 
 using json = nlohmann::json;
 
 using namespace std;
+namespace fs = std::filesystem;
+
 
 void DataManager::saveData(string repositoryName,  vector<TrackedFile> files, vector<unique_ptr<Commit>> commits) {
-    json object;
-    object["repositoryName"] = repositoryName;
-    // TODO: Add ways to dump ALL values related to both vectors into the json objects
-    json filesJson = json::array();
+
+    if (!fs::exists("projects/" + repositoryName)){
+        fs::create_directories("projects/" + repositoryName);
+    }
+    // files
+    for (auto& file : files){
+        string txtFilePath = "projects/" + repositoryName + "/" + file.getFileName();
+        ofstream txtFileWrite(txtFilePath);
+            if (txtFileWrite.is_open()){
+                txtFileWrite << file.getFileContent();
+                txtFileWrite.close();
+            }
+    }
+    
+    // json
+    std::ofstream metadataJson("metadata.json");
+    json projectMetadata;
+    string metaDataPath = "projects/"+repositoryName+"/" + "metadata.json";
+
+    ifstream metadataIn(metaDataPath);
+    if (metadataIn.is_open()) {
+        metadataIn >> projectMetadata;
+        metadataIn.close();
+    }
+    // write files
+    projectMetadata["repositoryName"] = repositoryName;
+    json filesMetadata = json::array();
+    time_t timestamp;
+    time(&timestamp);
     for (const auto& file : files) {
         json tempObject;
-        object["filePath"] = file->getFilePath();
-        object["fileName"] = file->getFileName();
-        object["content"] = file->getContent();
-        object["status"] = file->getStatus();
-        filesJson.push_back(std::move(tempObject));
+        tempObject["fileName"] = file.getFileName();
+        tempObject["filePath"] = file.getFilePath();
+        tempObject["lastModified"] = ctime(&timestamp);
+        filesMetadata.push_back(std::move(tempObject));
     }
-    object["filesTracked"] = std::move(filesJson);
+    projectMetadata["files"] = filesMetadata;
 
-    json commitsJson = json::array();
-    for (const auto& commit : commits) {
-        json tempObject;
-        object["id"] = commit->getId();
-        object["data"] = commit->getDate();
-        object["files"] = commit->getFiles();
-        object["messages"] = commit->getMessages();
-        commitsJson.push_back(std::move(tempObject));
+    // commits
+    json commitsData = json::array();
+    for (const auto& commit : commits){
+        json TempObj;
+        TempObj["id"] = commit.getId();
+        TempObj["date"] = commit.getDate();
+        TempObj["message"] = commit.getMessage();
+        commitsData.push_back(std::move(TempObj));
     }
-    object["filesTracked"] = std::move(commitsJson);
-
-    // placeholder file to write to, will be replaced with variable input
-    // to allow dynamic repos to be saved
-    std::ofstream out("data.json");
-    if (out.is_open()) {
-        out << std::setw(4) << object << std::endl;
-        out.close();
-    } else {
-        std::cerr << "cannot open json file for writing\n";
+       projectMetadata["commits"] = commitsData;
+    // metadata saving
+    ofstream metadataOut(metaDataPath);
+    if (metadataOut.is_open()) {
+        metadataOut << setw(4) << projectMetadata << endl;
+        metadataOut.close();
     }
+    // datahandler.json
 }
 
 bool loadData(const std::string& repositoryName, vector<TrackedFile> files, vector<unique_ptr<Commit>> commits) {
