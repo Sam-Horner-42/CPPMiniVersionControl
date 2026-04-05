63f0405311502396014f8b0ce40056c7a5b5a33b
Spencer Scarlett <scar0064@algonquinlive.com>
2026-04-05 16:37:12 -0400

    Made a generateID thing that I might not even need

--- includes/DataManager.h
+++ includes/DataManager.h
@@ -1,8 +1,17 @@
 #include <TrackedFile.h>
 #include <Commit.h>
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <filesystem>
+#include <sstream>
+#include <filesystem>
+#include <ctime>
+#include <string>
 
 class DataManager {
     public:
     void saveData(string repositoryName,  vector<TrackedFile> files, vector<unique_ptr<Commit>> commits);
     bool loadData(const std::string& repositoryName, vector<TrackedFile> files, vector<unique_ptr<Commit>> commits);
+    string generateId(string repositoryName, vector<TrackedFile> files);
 };
--- src/DataManager.cpp
+++ src/DataManager.cpp
@@ -1,19 +1,37 @@
 #include "../includes/DataManager.h"
 #include "../includes/nlohmann/json.hpp"
 
-#include <iostream>
-#include <fstream>
-#include <iomanip>
-#include <filesystem>
-#include <sstream>
-#include <filesystem>
-#include <ctime>
+
 
 using json = nlohmann::json;
-
 using namespace std;
 namespace fs = std::filesystem;
 
+
+string generateId(string repositoryName, vector<TrackedFile> files){
+
+    /*
+    using the FNV-1a hasing algorithm without extras
+    FowlerNollVo hash function, we need two magic large numbers(given I didn't make them), offset and prime
+    use them based off contents of the files, convert the hash to a string and take the first 10
+    */
+    unsigned long long hash = 14695981039346656037ULL;  // fnv-1a offset
+    unsigned long long prime = 1099511628211ULL; // fnv-1a prime
+    for (auto& file : files){
+            string txtFilePath = "projects/" + repositoryName + "/" + file.getFileName();
+            std::ifstream fileContentRead(txtFilePath, std::ios::binary);
+        char c;
+        while (fileContentRead.get(c)) {
+            hash = (hash ^ c) * prime;  
+        }
+        
+    }
+    
+    // Convert hash to string and take first 10 characters
+    string result = to_string(hash);
+    result = result.substr(0, 10);
+    return result;  
+}
 
 void DataManager::saveData(string repositoryName,  vector<TrackedFile> files, vector<unique_ptr<Commit>> commits) {
 
@@ -82,7 +100,7 @@ void DataManager::saveData(string repositoryName,  vector<TrackedFile> files, ve
 
     json projectItem;
     projectItem["name"] = repositoryName;
-    projectItem["id"] = 12345; // generateId(repositoryName); i need to back something for ids
+    projectItem["id"] = generateId(repositoryName, files);
     projectItem["path"] = "projects/" + repositoryName;
     
 }
