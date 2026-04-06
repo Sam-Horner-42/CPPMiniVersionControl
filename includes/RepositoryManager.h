cdc5cdb332885b278d2ff682b812ac4f942ef166
Ethan Geary <gear0020@algonquincollege.com>
2026-04-06 18:23:43 -0400

    i hope all my fixes stay the second time around

--- includes/RepositoryManager.h
+++ includes/RepositoryManager.h
@@ -15,20 +15,18 @@
 #include "../includes/StandardCommit.h"
 #include "../includes/DataManager.h"
 
-#include <string>
-
 class RepositoryManager {
   private:
-    Repository::Repository repo;
-    DataManager::DataManager data;
+    Repository repo;
+    DataManager data;
   public:
     RepositoryManager (Repository& repo);
     ~RepositoryManager();
-    Repository::Repository RepositoryManager::createRepository(const std::string& repoName);
+    Repository RepositoryManager::createRepository(const std::string& repoName);
     void loadRepostiory();
-    void saveRepository();
+    bool saveRepository();
     Commit* searchCommits(const std::string& searchString);
-    void getFileStatus(const TrackedFile::TrackedFile& file);
+    void getFileStatus(const TrackedFile& file);
 
     void restore(const std::string& commitId);
     Commit* getParentCommit(const std::string& commitId);
