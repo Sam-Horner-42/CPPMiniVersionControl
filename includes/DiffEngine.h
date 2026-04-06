458ea0ad03e4a84ecd6d3a8d80b61552f25e3156
JDMTL5150 <jacobd2887@gmail.com>
2026-04-06 15:19:34 -0400

    minor fix to diff engine h

--- includes/DiffEngine.h
+++ includes/DiffEngine.h
@@ -3,15 +3,6 @@
  * Author: Jacob Dawes - 041169788
  */
 
-<<<<<<< HEAD
-#ifndef DIFF_ENGINE_H
-#define DIFF_ENGINE_H
-
-class DiffEngine {
-  public:
-    string computeDiff(const std::vector<std::string>& currentContent, const std::vector<std::string>& oldContent);
-    void displayDiff();
-=======
 #ifndef DIFFENGINE_H
 #define DIFFENGINE_H
 
@@ -20,7 +11,6 @@ class DiffEngine {
   public:
     void computeDiff(const std::vector<std::string>& currentContent, const std::vector<std::string>& oldContent);
     std::string displayDiff() const;
->>>>>>> 9fd035e (changed hpp to h, worked on repo and diffEngine classes - Jacob)
 };
 
 #endif
