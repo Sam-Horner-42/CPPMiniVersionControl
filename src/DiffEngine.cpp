4efbce4a04a77457528c7562556d5fdf9e510445
Jacob Dawes <jacobd2887@gmail.com>
2026-04-06 14:11:05 -0400

    repo manager work

--- src/DiffEngine.cpp
+++ src/DiffEngine.cpp
@@ -7,45 +7,39 @@
 #include <vector>
 #include <iostream>
 
-#include "../includes/Commit.h"
+#include "../includes/StandardCommit.h"
 #include "../includes/DiffEngine.h"
 
 using namespace std;
 
 void DiffEngine::computeDiff(const vector<string>& currentContent, const vector<string>& oldContent) {
-  // defensive checks
-  if(currentContent.size() < 1 || oldContent.size() < 1) return nullptr;
+    if(currentContent.empty() || oldContent.empty()) return;
 
-  // find the smallest file for diffing
-  int minSize = currentContent.size() < oldContent.size() ? currentContent.size() : oldContent.size();
-  int linesModified = 0;
+    int minSize = min(currentContent.size(), oldContent.size());
+    string diffString = "";
 
-  string diffString = "";
-
-  // compute the diff
-  for(auto i = 0; i < minSize; i++) {
-    if(currentContent[i] != oldContent[i]) {
-      linesModified++;
-      diffString += "- " + oldContent[i] + "\n";
-      diffString += "+ " + currentContent[i] + "\n";
+    for(auto i = 0; i < minSize; i++) {
+        if(currentContent[i] != oldContent[i]) {
+            linesModified++;
+            diffString += "- " + oldContent[i] + "\n";
+            diffString += "+ " + currentContent[i] + "\n";
+        }
     }
-  }
 
-  // check for different sized files
-  // indicate add or removal depending on the file version.
-  if(currentContent.size() > oldContent.size()) {
-    for(auto i = minSize; i < currentContent.size(); i++) {
-      diffString += "+ " + currentContent[i] + "\n";
+    if(currentContent.size() > oldContent.size()) {
+        for(auto i = minSize; i < currentContent.size(); i++) {
+            linesAdded++;
+            diffString += "+ " + currentContent[i] + "\n";
+        }
     }
-  }
-  else if(currentContent.size() < oldContent.size()) {
-    for(auto i = minSize; i < oldContent.size(); i++) {
-      diffString += "- " + currentContent[i] + "\n";
+    else if(currentContent.size() < oldContent.size()) {
+        for(auto i = minSize; i < oldContent.size(); i++) {
+            linesRemoved++;  
+            diffString += "- " + oldContent[i] + "\n"; 
+        }
     }
-  }
 
-  // set computed diff string
-  this->diffString = diffString;
+    this->diffString = diffString;
 }
 
 string DiffEngine::displayDiff() {
