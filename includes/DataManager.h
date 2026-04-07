2ea2522d87a008aeae0f2409e2ed83657c686882
Spencer Scarlett <scar0064@algonquinlive.com>
2026-04-07 11:02:05 -0400

    Updated Single hash

--- includes/DataManager.h
+++ includes/DataManager.h
@@ -19,7 +19,7 @@ using namespace std;
 class DataManager {
 public:
     string generateId(string repositoryName, vector<TrackedFile> files);
-    string singleHash(string repositoryName, string singleFile);
+    string singleHash(string fileContents);
     void saveData(string repositoryName);
     bool loadData(const string& repositoryName);
 };
--- src/DataManager.cpp
+++ src/DataManager.cpp
@@ -11,15 +11,12 @@ namespace fs = std::filesystem;
 /*
 
 */
-string DataManager::singleHash(string repositoryName, string fileName){
+string DataManager::singleHash(string fileContents){
     unsigned long long hash = 14695981039346656037ULL;  // fnv-1a offset
     unsigned long long prime = 1099511628211ULL; // fnv-1a prime
-    string txtFilePath = "projects/" + repositoryName + "/" + fileName;
-    std::ifstream fileContentRead(txtFilePath, std::ios::binary);
-    char c;
-    while (fileContentRead.get(c)) {
-        hash = (hash ^ c) * prime;  
-    }
+    for (char c : fileContents)
+        hash = (hash ^ c) * prime;  // updated so that it just reads a fileContent string and applies hash
+
     string result = to_string(hash);
     result = result.substr(0, 10);
     return result;  
